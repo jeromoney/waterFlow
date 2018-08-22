e294f506600b9420deef054b6e172f1f1116d6dc
Justin Matis <justin.matis@gmail.com>
2018-08-22 14:49:16 -0600

    Made video container responsive for small devices

--- js/index.js
+++ js/index.js
@@ -75,6 +75,35 @@ for (var i = 0; i < inputs.length; i++) {
     inputs[i].onclick = hideFeature;
 }
 
+layerList = document.getElementById('difficulty-hide-buttons');
+inputs = layerList.getElementsByTagName('input');
+
+function hideDifficulty(button){
+    // hides the put-ins and rivers segments based on class
+    // Uses a filter to show only a fraction of each layer
+    // convert roman numeral to numeric
+    let roman_numeral = this.value;
+    let roman_numeral_array = ['I','II','III','IV','V'];
+    for (var i = 0; i < roman_numeral_array.length; i++) {
+        if (roman_numeral == roman_numeral_array[i]) {break;};
+    }
+    i = i * 3 + 1;
+    let difficult_arry = [i - 1, i , i +1]; // Includes +/- difficulties
+    let layers = ['rivers-dry','rivers-flowing','putins']
+    for (var i = 0; i < layers.length; i++) {
+        var layer = layers[i];
+        filter = map.getFilter(layer);
+        // The toggle state is stored in the presence/absence of the difficult_arry in the filter
+        filter[2][1][2] = 3+0.1;
+        filter[2][2][2] = 3-0.1;
+        map.setFilter(layer, filter);
+    }
+}
+
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].onclick = hideDifficulty;
+}
+
 map.on('styledata', function(e) {
     // Waits for style to be loaded and then filters out relevent features.
     // style is being loaded 3 times for some reason
@@ -86,7 +115,7 @@ map.on('styledata', function(e) {
 
 // click on map and get information about feature
 map.on('click', function(e) {
-    var box = 10;
+    var box = 20;
     let myLayers = ['rivers-running','rivers-dry','rivers-nonww','videos','gauge','rivercount','putins'];
     var features = map.queryRenderedFeatures([[e.point.x - box, e.point.y - box], [e.point.x + box, e.point.y + box]], {
     layers: null // myLayers
@@ -118,7 +147,7 @@ map.on('click', function(e) {
         popup.setHTML('<h3><a href="https://www.americanwhitewater.org/content/River/detail/id/'+ prop.awid +'/">' + prop.gnis_name + ' </a></h3>' +
         (prop.level  !== '' ?  '<div>level: ' + prop.level +'</div>': "")+
         '<div>class: ' + prop.difficulty + '</div>'+
-        (prop.flow  !== '' ?  '<div>flow: ' + prop.flow +' cfs</div>': ""))+
+        (prop.flow  !== '' ?  '<div>flow: ' + prop.flow +' cfs</div>': ""))+// Don't understand why I need two parentheses, but it breaks otherwise
         '<div>gradient: ' + prop.slope + ' fpm</div>';
         break;
     case myLayers[1]:
@@ -126,7 +155,7 @@ map.on('click', function(e) {
         popup.setHTML('<h3><a href="https://www.americanwhitewater.org/content/River/detail/id/'+ prop.awid +'/">' + prop.gnis_name + ' </a></h3>' +
         (prop.level  !== '' ?  '<div>level: ' + prop.level +'</div>': "")+
         '<div>class: ' + prop.difficulty + '</div>'+
-        (prop.flow  !== '' ?  '<div>flow: ' + prop.flow +' cfs</div>': ""))+
+        (prop.flow  !== '' ?  '<div>flow: ' + prop.flow +' cfs</div>': ""))+ // Don't understand why I need two parentheses, but it breaks otherwise
         '<div>gradient: ' + prop.slope + ' fpm</div>';
         break;
     case myLayers[2]:
@@ -137,7 +166,8 @@ map.on('click', function(e) {
         break;
     case myLayers[3]:
     // video
-        popup.setHTML('<iframe width="640" height="360" allowfullscreen="allowfullscreen" src="https://www.youtube.com/embed/'+ prop.videoid +'"></iframe>');
+        popup.setHTML('<iframe width="'+Math.min(480,$(window).width())+'" height="'+Math.min(360,$(window).height())+'" allowfullscreen="allowfullscreen" src="https://www.youtube.com/embed/'+ prop.videoid +'"></iframe>');
+        popup.options.offset = null;
         break;
     case myLayers[4]:
     // gauge
@@ -165,7 +195,7 @@ map.addControl(new MapboxGeocoder({
 
 function hideDiffRivers() {
     // hides Rivers based on difficulty ratings
-    var layers = ['rivers-dry','rivers-flowing','putins']
+    let layers = ['rivers-dry','rivers-flowing','putins']
     var arrayLength = layers.length;
     for (var i = 0; i < arrayLength; i++) {
         var layer = layers[i];
